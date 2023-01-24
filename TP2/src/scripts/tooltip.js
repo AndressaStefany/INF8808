f9d4adf1831aff37db568ad32d7bcb47377f1516
lotfi27 <71388054+lotfi27@users.noreply.github.com>
2023-01-23 19:34:44 -0500

    Update tooltip.js

--- TP2/src/scripts/tooltip.js
+++ TP2/src/scripts/tooltip.js
@@ -5,15 +5,23 @@
  * @returns {string} The tooltip contents
  */
 export function getContents (d) {
-  /* TODO : Define and return the tooltip contents including :
-      + A title stating the hovered element's group, with:
-        - Font family: Grenze Gotish
-        - Font size: 24px
-        - Font weigth: normal
-      + A bold label for the player name followed
-        by the hovered elements's player's name
-      + A bold label for the player's line count
-        followed by the number of lines
-  */
-  return ''
+  const tooltip = d3.create()
+
+  tooltip.append('div')
+    .append('text')
+    .style('font-size', '24px')
+    .style('font-family', 'Grenze Gotish')
+    .style('font-weight', 'bold')
+    .text(d.target.__data__.Player.Player)
+    .append('p')
+
+  tooltip.append('div')
+    .append('text')
+    .style('font-weight', 'normal')
+    .text(d.target.__data__.Player.Count)
+    .append('b')
+    .style('font-weight', 'normal')
+    .text(' lines')
+
+  return tooltip.html()
 }
