1c269d0c2fb2fdacd852eed9aac83ea2bdd34b98
lotfi27 <71388054+lotfi27@users.noreply.github.com>
2023-03-20 16:31:14 -0400

    lint legends

--- TP4/src/scripts/legend.js
+++ TP4/src/scripts/legend.js
@@ -8,17 +8,16 @@ import d3Legend from 'd3-svg-legend'
  * @param {number} width The width of the graph, used to place the legend
  */
 export function drawLegend (colorScale, g, width) {
-  const domain = colorScale.domain().sort();
-  colorScale.domain(domain);
-
+  const domain = colorScale.domain().sort()
+  colorScale.domain(domain)
 
   const legend = d3Legend.legendColor()
     .title('Legend')
     .shape('path', d3.symbol().type(d3.symbolCircle).size(300)())
-    .scale(colorScale);
+    .scale(colorScale)
 
   g.append('g')
     .attr('class', 'legend')
     .attr('transform', `translate(${width}, -20)`)
-    .style('font-size', '12px').call(legend);
+    .style('font-size', '12px').call(legend)
 }
