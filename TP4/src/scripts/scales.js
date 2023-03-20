ded23a45af56e18a478c9432c08dedc56fdfd5a2
lotfi27 <71388054+lotfi27@users.noreply.github.com>
2023-03-20 00:08:44 -0400

    Update scales.js

--- TP4/src/scripts/scales.js
+++ TP4/src/scripts/scales.js
@@ -9,8 +9,11 @@
  * @returns {*} The linear scale used to determine the radius
  */
 export function setRadiusScale (data) {
-  // TODO : Set scale
-  return {}
+  const populations = [...data['2000'], ...data['2015']].map((d) => d.Population);
+  
+  const populationScale = d3.scaleLinear().domain([d3.min(populations), d3.max(populations)]).range([5,20])
+
+  return populationScale
 }
 
 /**
@@ -24,8 +27,17 @@ export function setRadiusScale (data) {
  * @returns {*} The ordinal scale used to determine the color
  */
 export function setColorScale (data) {
-  // TODO : Set scale
-  return {}
+  const continents = new Set();
+
+  [...data['2000'], ...data['2015']].forEach((country) => {
+    continents.add(country.Continent);
+  });
+  
+  const continentScale = d3.scaleOrdinal()
+    .domain([...continents].sort())
+    .range(d3.schemeCategory10);
+  
+  return continentScale;
 }
 
 /**
@@ -36,8 +48,13 @@ export function setColorScale (data) {
  * @returns {*} The linear scale in X
  */
 export function setXScale (width, data) {
-  // TODO : Set scale
-  return {}
+  const gdps = [...data['2000'], ...data['2015']].map((d) => d.GDP);
+
+  const gdpScale = d3.scaleLog()
+    .domain([d3.min(gdps), d3.max(gdps)])
+    .range([0, width]);
+  
+  return gdpScale;
 }
 
 /**
@@ -48,6 +65,11 @@ export function setXScale (width, data) {
  * @returns {*} The linear scale in Y
  */
 export function setYScale (height, data) {
-  // TODO : Set scale
-  return {}
+  const co2s = [...data['2000'], ...data['2015']].map((d) => d.CO2);
+
+  const co2Scale = d3.scaleLog()
+    .domain([d3.min(co2s), d3.max(co2s)])
+    .range([height, 0]);
+  
+  return co2Scale;
 }
