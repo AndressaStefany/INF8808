6c255fd28368cd208931faccb84df8626864905d
lotfi27 <71388054+lotfi27@users.noreply.github.com>
2023-03-20 00:25:14 -0400

    Update legend.js

--- TP4/src/scripts/legend.js
+++ TP4/src/scripts/legend.js
@@ -8,6 +8,17 @@ import d3Legend from 'd3-svg-legend'
  * @param {number} width The width of the graph, used to place the legend
  */
 export function drawLegend (colorScale, g, width) {
-  // TODO : Draw the legend using d3Legend
-  // For help, see : https://d3-legend.susielu.com/
+  const domain = colorScale.domain().sort();
+  colorScale.domain(domain);
+
+
+  const legend = d3Legend.legendColor()
+    .title('Legend')
+    .shape('path', d3.symbol().type(d3.symbolCircle).size(300)())
+    .scale(colorScale);
+
+  g.append('g')
+    .attr('class', 'legend')
+    .attr('transform', `translate(${width}, -20)`)
+    .style('font-size', '12px').call(legend);
 }
