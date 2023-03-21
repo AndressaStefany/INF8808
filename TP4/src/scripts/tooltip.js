483e3897617b122c2a9329b8d1922689cb6dd8f2
lotfi27 <71388054+lotfi27@users.noreply.github.com>
2023-03-20 20:49:57 -0400

    Update tooltip.js

--- TP4/src/scripts/tooltip.js
+++ TP4/src/scripts/tooltip.js
@@ -7,6 +7,36 @@
  * @returns {string} The tooltip contents
  */
 export function getContents (d) {
-  // TODO : Generate tooltip contents
-  return ''
+  const formatNumber = (num) => {
+    const formatter = new Intl.NumberFormat('en-US', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    })
+    return formatter.format(num)
+  }
+
+  const createDataItem = (label, value) => ({ label, value })
+
+  const data = [
+    createDataItem('Country:', d['Country Name']),
+    createDataItem('Population:', d.Population),
+    createDataItem('GDP:', formatNumber(d.GDP) + ' $ (USD)'),
+    createDataItem('CO2 emissions:', formatNumber(d.CO2) + ' metric tonnes')
+  ]
+
+  const createContent = (data) => {
+    const container = d3.create('div')
+    data.forEach(({ label, value }) => {
+      const itemContainer = container.append('div')
+      itemContainer.append('span')
+        .attr('class', 'tooltip-label')
+        .text(label)
+      itemContainer.append('span')
+        .attr('class', 'tooltip-value')
+        .text(value)
+    })
+    return container.html()
+  }
+
+  return createContent(data)
 }
