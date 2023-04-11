6502199b7c415e08d8bc2587c2e737a7fa2276d9
lotfi27 <71388054+lotfi27@users.noreply.github.com>
2023-04-10 21:22:47 -0400

    Update viz.js

--- TP5/src/scripts/viz.js
+++ TP5/src/scripts/viz.js
@@ -6,6 +6,8 @@
  */
 export function colorDomain (color, data) {
   // Set the color domain
+  const types = new Set(data.features.map(feature => feature.properties.TYPE_SITE_INTERVENTION))
+  color.domain([...types])
 }
 
 /**
@@ -17,6 +19,25 @@ export function colorDomain (color, data) {
  */
 export function mapBackground (data, path, showMapLabel) {
   // TODO : Generate the map background and set the hover handlers
+  const regions = d3.select('#map-g')
+    .selectAll('.region')
+    .data(data.features)
+
+  regions.enter()
+    .append('g')
+    .attr('class', 'region')
+    .append('path')
+    .attr('d', path)
+    .attr('fill', '#fff')
+    .attr('stroke', '#a7a7a0')
+    .on('mouseover', (mouseEvent, d) => {
+      showMapLabel(d, path)
+    })
+    .on('mouseout', () => {
+      d3.select('.main-svg').select('.mapLabel').remove()
+    })
+
+  regions.exit().remove()
 }
 
 /**
@@ -30,6 +51,16 @@ export function mapBackground (data, path, showMapLabel) {
 export function showMapLabel (d, path) {
   // TODO : Show the map label at the center of the neighborhood
   // by calculating the centroid for its polygon
+  const [x, y] = path.centroid(d)
+
+  d3.select('.main-svg')
+    .append('text')
+    .attr('class', 'mapLabel')
+    .attr('x', x)
+    .attr('y', y)
+    .attr('text-anchor', 'middle')
+    .style('font', '11px "Open Sans Condensed"')
+    .text(d.properties.NOM)
 }
 
 /**
@@ -40,8 +71,24 @@ export function showMapLabel (d, path) {
  * @param {*} panel The display panel, which should be dislayed when a circle is clicked
  */
 export function mapMarkers (data, color, panel) {
-  // TODO : Display the map markers.
-  // Their color corresponds to the type of site and their outline is white.
-  // Their radius is 5 and goes up to 6 while hovered by the cursor.
-  // When clicked, the panel is displayed.
+  d3.select('#marker-g')
+    .selectAll('.marker')
+    .data(data.features)
+    .join('circle')
+    .attr('class', 'marker')
+    .attr('r', 5)
+    .each(function (d) {
+      const [x, y] = d.geometry.coordinates
+      d3.select(this).attr('cx', x).attr('cy', y)
+    })
+    .attr('fill', d => color(d.properties.TYPE_SITE_INTERVENTION))
+    .on('mouseover', function () {
+      d3.select(this).attr('r', 6)
+    })
+    .on('mouseout', function () {
+      d3.select(this).attr('r', 5)
+    })
+    .on('click', function (mouseEvent, d) {
+      panel.display(d, color)
+    })
 }
